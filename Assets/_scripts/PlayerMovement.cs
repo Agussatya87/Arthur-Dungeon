a01a05f8d15949196d5f0225608a8ccf7f034679
satya <satyapratama373@gmail.com>
2023-07-26 20:31:43 +0800

    update level

--- Assets/_scripts/PlayerMovement.cs
+++ Assets/_scripts/PlayerMovement.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private int maxJumps = 2;
+    private int jumpsRemaining;
+
     private Rigidbody2D body;
     private SpriteRenderer spriteRenderer;
     private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        jumpsRemaining = maxJumps;
     }
 
     private void Update()
@@ -27,10 +33,43 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontalInput < -0.01f)
             spriteRenderer.flipX = true;
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (grounded)
+            {
+                Jump();
+                jumpsRemaining = maxJumps - 1;
+            }
+            else if (jumpsRemaining > 0)
+            {
+                Jump();
+                jumpsRemaining--;
+            }
+        }
 
         //Set animator
         anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, jumpHeight);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+            jumpsRemaining = maxJumps;
+        }
+        else if (collision.gameObject.tag == "Rintangan")
+        {
+            grounded = true;
+            jumpsRemaining = maxJumps;
+        }
     }
 }
